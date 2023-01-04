a6effe4257f657eb785ba7254e400c9ea8e3fe21
DevAdedeji <adedejitewogbade2@gmail.com>
2023-01-04 18:15:32 +0100

    Done with login

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const errorHandler = require('../utils/errorHandler');
 const bcrypt = require('bcrypt');
+const createToken = require('../utils/createToken');
 
 
 const register = async (req, res)=>{
@@ -17,8 +18,28 @@ const register = async (req, res)=>{
     }
 }
 
-const login = (req, res)=>{
-    
+const login = async (req, res)=>{
+    const {email, password} = req.body
+
+    try{
+        const user = await User.findOne({email})
+        if(user){
+            const auth = await bcrypt.compare(password, user.password);
+            if(auth){
+                const {password, ...others} = user._doc;
+                const token = createToken(user.email);
+                res.status(200).json({user:others, status:true, token});
+            }else{
+                res.status(400).json({error:"Password not correct"})
+            }
+        }else{
+            res.status(400).json({error:"Invalid Credentials"})
+        }
+    }catch(err){
+        const error = errorHandler(err)
+        const {status, ...others} = error;
+        res.status(status || 500).json({error:others || 'Somethng went wrong, pls try again'});
+    }
 }
 
 const getUser = (req, res) =>{
