c3a33f5c948935ab0c7e7b4e6cfdf3a490871181
DevAdedeji <adedejitewogbade2@gmail.com>
2023-01-04 18:07:39 +0100

    Done with the register endpoint

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,5 +1,20 @@
-const register = (req, res)=>{
-    
+const User = require('../models/User');
+const errorHandler = require('../utils/errorHandler');
+const bcrypt = require('bcrypt');
+
+
+const register = async (req, res)=>{
+    const {username, password, email} = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    try{
+        const user = await User.create({username, email, password:hashedPassword});
+        res.status(201).json({status:true, message:"User created successfully"})
+    }catch(err){
+        const error = errorHandler(err);
+        const {status, ...others} = error;
+        res.status(status).json({error:others});
+    }
 }
 
 const login = (req, res)=>{
