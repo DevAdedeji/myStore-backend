ba12ef95293aebcc49b31a9630a0851f06a98fe9
DevAdedeji <adedejitewogbade2@gmail.com>
2023-01-05 17:36:54 +0100

    Added featured images product model

--- models/Product.js
+++ models/Product.js
@@ -27,6 +27,10 @@ const productSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
+    featuredImages:{
+        type:Array,
+        required:false,
+    },
     categories:{
         type:Array,
         required:true,
